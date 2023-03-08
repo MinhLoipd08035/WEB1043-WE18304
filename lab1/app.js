660d1b5caf581294486d5076700e6801632667a5
MinhLoipd08035 <lointmpd08035@fpt.edu.vn>
2023-03-08 21:31:59 +0700

    noplab1

---
+++ lab1/app.js
@@ -0,0 +1,14 @@
+
+// nhập số a từ bàn phím
+
+let a = prompt("Mời bạn nhập số nguyên a:");// let a1= parseInt(a);
+
+// nhập b từ bàn phím
+let b =prompt("Mời bạn nhập số nguyên b:");
+
+let c = parseInt(a)+parseInt(b);
+//in ra màn hình
+
+document.write(c);
+// show giá trị biến
+console.log(c);
